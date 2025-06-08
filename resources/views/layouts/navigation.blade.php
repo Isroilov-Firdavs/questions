3eba7bdb229d93351ef8ac8309acebd7f769fb63
Isroilov Firdavs <firdavsisroilov28@gmail.com>
2025-06-09 01:43:40 +0500

    user

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -5,14 +5,14 @@
             <div class="flex">
                 <!-- Logo -->
                 <div class="shrink-0 flex items-center">
-                    <a href="{{ route('dashboard.user') }}">
+                    <a href="{{ route('user') }}">
                         <x-application-logo class="block h-9 w-auto fill-current text-gray-800 dark:text-gray-200" />
                     </a>
                 </div>
 
                 <!-- Navigation Links -->
                 <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
-                    <x-nav-link :href="route('dashboard.user')" :active="request()->routeIs('dashboard.user')">
+                    <x-nav-link :href="route('user')" :active="request()->routeIs('dashboard.user')">
                         {{ __('Dashboard') }}
                     </x-nav-link>
                 </div>
